4ac41c7c0932eca8aed32ccf10d97422f5a6a27a
Rahul Parmar <rahulparmar.n@gmail.com>
2020-07-19 14:48:40 +0530

    Update AccountController.cs

--- IdentityAuthServer/Controllers/AccountController.cs
+++ IdentityAuthServer/Controllers/AccountController.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityAuthServer.Controllers
 {
-    public class AccountController
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
     {
+        [HttpGet]
+        [Route("test")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+            })
+            .ToArray();
+        }
     }
 }
