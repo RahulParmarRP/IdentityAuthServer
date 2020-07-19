dd49f88944b3dfba879c3182da842de0f3b012c2
Rahul Parmar <rahulparmar.n@gmail.com>
2020-07-19 21:33:46 +0530

    updated example web api

--- TestWebAPI/Controllers/WeatherForecastController.cs
+++ TestWebAPI/Controllers/WeatherForecastController.cs
@@ -9,16 +9,16 @@ namespace TestWebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class TestWebApiController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<TestWebApiController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public TestWebApiController(ILogger<TestWebApiController> logger)
         {
             _logger = logger;
         }
