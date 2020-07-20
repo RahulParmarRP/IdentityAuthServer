717f9e7d3ab450c6218817ff9b295e3ec8823a7d
Rahul Parmar <rahulparmar.n@gmail.com>
2020-07-20 20:52:31 +0530

    Create IdentityController.cs

---
+++ TestWebAPI/Controllers/IdentityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+            })
+            .ToArray();
+        }
+    }
+}
