126299df265b443cefce81fd98a54d37fd1f7064
Rahul Parmar <rahulparmar.n@gmail.com>
2020-07-25 13:18:53 +0530

    Update Startup.cs

--- TestWebAPI/Startup.cs
+++ TestWebAPI/Startup.cs
@@ -29,23 +29,33 @@ namespace TestWebAPI
             services.AddControllers();
 
             /*
-             * 
              * Authentication with an additional AddIdentityServerJwt helper method that configures the app to validate JWT tokens produced by IdentityServer:
                 services.AddAuthentication()
                     .AddIdentityServerJwt();
              */
 
             // accepts any access token issued by identity server
-            services.AddAuthentication("Bearer")
-                 .AddJwtBearer("Bearer", options =>
+            services.AddAuthentication("JWTBearerToken") // custom scheme name
+                 .AddJwtBearer("JWTBearerToken", options =>
                  {
                      options.Authority = "https://localhost:5001";
-
-                     options.TokenValidationParameters = new TokenValidationParameters
+                     options.TokenValidationParameters =
+                     new TokenValidationParameters
                      {
                          ValidateAudience = false
                      };
                  });
+
+            // adds an authorization policy to make sure the token is for scope 'api1'
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AuthorizeByApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "api1");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,7 +79,10 @@ namespace TestWebAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints
+                .MapControllers()
+                // auth policy name
+                .RequireAuthorization("AuthorizeByApiScope");
             });
         }
     }
