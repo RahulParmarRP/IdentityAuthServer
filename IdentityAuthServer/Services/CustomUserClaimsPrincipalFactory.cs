f865f4303d9ba958b9385919af9f8ef838f3d559
Rahul Parmar <rahulparmar.n@gmail.com>
2020-08-02 18:22:01 +0530

    Create CustomUserClaimsPrincipalFactory.cs

---
+++ IdentityAuthServer/Services/CustomUserClaimsPrincipalFactory.cs
@@ -0,0 +1,34 @@
+﻿using IdentityAuthServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityAuthServer.Services
+{
+    public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<AppUser>
+    {
+        public CustomUserClaimsPrincipalFactory(
+            UserManager<AppUser> userManager,
+            IOptions<IdentityOptions> optionsAccessor
+            ) : base(userManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(AppUser user)
+        {
+            var claimsIdentity = await base.GenerateClaimsAsync(user);
+            //if (!claimsIdentity.HasClaim(x => x.Type == JwtClaimTypes.Subject))
+            //{
+            //    var sub = user.SubjectId;
+            //    claimsIdentity.AddClaim(new Claim(JwtClaimTypes.Subject, sub));
+            //}
+            // add custom claims
+            claimsIdentity.AddClaim(new Claim("TestClaim", "role"));
+            return claimsIdentity;
+        }
+    }
+}
