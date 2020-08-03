561227d5a68b76b190d9532b61e51c1defa07982
Rahul Parmar <rahulparmar.n@gmail.com>
2020-08-04 01:20:49 +0530

    Update RegisterViewModel.cs

--- IdentityAuthServer/ViewModel/RegisterViewModel.cs
+++ IdentityAuthServer/ViewModel/RegisterViewModel.cs
@@ -23,5 +23,8 @@ namespace IdentityAuthServer.ViewModel
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Display(Name = "UserRole")]
+        public string UserRole { get; set; }
     }
 }
