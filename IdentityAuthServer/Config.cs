40c32a54baa7e367575a03c00a6d3783e58b8c38
Rahul Parmar <rahulparmar.n@gmail.com>
2020-08-04 01:21:42 +0530

    Update Config.cs

--- IdentityAuthServer/Config.cs
+++ IdentityAuthServer/Config.cs
@@ -3,29 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace IdentityAuthServer
 {
     public static class Config
     {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource()
+                {
+                    Name = "customRoleClaim",
+                    DisplayName="Your profile data",
+                    UserClaims =
+                    {
+                        "UserRole",
+                        "TestRole",
+                        "roleType"
+                    }
+                }
+            };
+        }
+
         public static IEnumerable<ApiResource> GetApiResources()
         {
+            var efeResourceClaimTypes = new List<string>
+            {
+                JwtClaimTypes.Name,
+                JwtClaimTypes.Email,
+                JwtClaimTypes.Profile,
+                "UserRole","TestRole","roleType"
+            };
+
             return new List<ApiResource>
             {
-                new ApiResource("myresourceapi", "My Resource API")
-                {
-                    Scopes = {"api"}
-                }
+                new ApiResource("api1", "API1", efeResourceClaimTypes)
             };
         }
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
             {
-                new ApiScope("api1", "My API")
+                new ApiScope("api1", "My API"),
+                new ApiScope(name: "read",   displayName: "Read your data."),
+                new ApiScope(name: "write",  displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data.")
             };
 
         public static IEnumerable<Client> Clients =>
@@ -43,7 +74,10 @@ namespace IdentityAuthServer
                         new Secret("secret".Sha256())
                     },
                     // scopes that client has access to
-                    AllowedScopes = { "api1" },
+                    AllowedScopes =
+                    {
+                        "api1"
+                    },
                 },
                 /*
                  * The presence (or absence) of the sub claim lets the API 
@@ -53,6 +87,9 @@ namespace IdentityAuthServer
                 // for role claims
                 new Client
                 {
+                    AllowOfflineAccess = true,
+                    AlwaysSendClientClaims = true,
+                    AlwaysIncludeUserClaimsInIdToken = true,
                     ClientId = "clientId_for_claims_based_api",
                     // now the user requires username and password
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
@@ -62,9 +99,25 @@ namespace IdentityAuthServer
                         new Secret("secret".Sha256())
                     },
                     // scopes that client has access to
-                    AllowedScopes = { "api1" },
-                },
+                    AllowedScopes =
+                    {
+                        "api1",
+                        StandardScopes.Profile,
+                        StandardScopes.Email,
+                        StandardScopes.OpenId,
+                        //"read", "write", "delete",
+                        "customRoleClaim"
+                    },
 
+                    /*AlwaysSendClientClaims
+                     * If set, the client claims will be sent for every flow. 
+                     * If not, only for client credentials flow (default is false)
+                     */
+                    Claims =
+                    {
+                        new ClientClaim("roleType","ClientAdmin")
+                    }
+                },
                 new Client
                 {
                     ClientId = "custom_claims",
@@ -73,13 +126,15 @@ namespace IdentityAuthServer
                     {
                         new Secret("secret".Sha256())
                     },
-                    AllowedScopes = { "ApiThree" },
+                    AllowedScopes =
+                    {
+                        "ApiThree"
+                    },
                     Claims =
                     {
-                        new ClientClaim("role","ClientAdmin")
+                        new ClientClaim("roleType","ClientAdmin")
                     }
                 },
-
                 new Client
                 {
                     ClientId = "openid",
