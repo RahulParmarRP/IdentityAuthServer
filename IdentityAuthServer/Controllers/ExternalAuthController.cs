8922f254f33ac5fac94574f01376e637aa7039eb
Rahul Parmar <rahulparmar.n@gmail.com>
2020-08-08 16:04:49 +0530

    Create ExternalAuthController.cs

---
+++ IdentityAuthServer/Controllers/ExternalAuthController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAuthServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExternalAuthController : ControllerBase
+    {
+        /*
+         * Verify the integrity of the ID token
+            After you receive the ID token by HTTPS POST, you must verify the integrity of the token. To verify that the token is valid, ensure that the following criteria are satisfied:
+            The ID token is properly signed by Google. Use Google's public keys (available in JWK or PEM format) to verify the token's signature. These keys are regularly rotated; examine the Cache-Control header in the response to determine when you should retrieve them again.
+            The value of aud in the ID token is equal to one of your app's client IDs. This check is necessary to prevent ID tokens issued to a malicious app being used to access data about the same user on your app's backend server.
+            The value of iss in the ID token is equal to accounts.google.com or https://accounts.google.com.
+            The expiry time (exp) of the ID token has not passed.
+            If you want to restrict access to only members of your G Suite domain, verify that the ID token has an hd claim that matches your G Suite domain name.
+            Rather than writing your own code to perform these verification steps, we strongly recommend using a Google API client library for your platform, or a general-purpose JWT library. For development and debugging, you can call our tokeninfo validation endpoint.
+         */
+        [HttpPost]
+        [Route("authgoogle")]
+        public async Task<IActionResult> AuthenticateGoogleSignin(string idToken)
+        {
+
+            /*
+             * After you have verified the token, check if the user is already in your user database. If so, establish an authenticated session for the user. If the user isn't yet in your user database, create a new user record from the information in the ID token payload, and establish a session for the user. You can prompt the user for any additional profile information you require when you detect a newly created user in your app
+             */
+
+            /*
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+
+...
+
+GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+    // Specify the CLIENT_ID of the app that accesses the backend:
+    .setAudience(Collections.singletonList(CLIENT_ID))
+    // Or, if multiple clients access the backend:
+    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+    .build();
+
+// (Receive idTokenString by HTTPS POST)
+
+GoogleIdToken idToken = verifier.verify(idTokenString);
+if (idToken != null) {
+  Payload payload = idToken.getPayload();
+
+  // Print user identifier
+  String userId = payload.getSubject();
+  System.out.println("User ID: " + userId);
+
+  // Get profile information from payload
+  String email = payload.getEmail();
+  boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+  String name = (String) payload.get("name");
+  String pictureUrl = (String) payload.get("picture");
+  String locale = (String) payload.get("locale");
+  String familyName = (String) payload.get("family_name");
+  String givenName = (String) payload.get("given_name");
+
+  // Use or store profile information
+  // ...
+
+} else {
+  System.out.println("Invalid ID token.");
+}
+             */
+            return Ok();
+        }
+    }
+}
