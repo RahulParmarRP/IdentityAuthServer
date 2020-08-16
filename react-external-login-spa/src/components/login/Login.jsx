074df3b6a70cb4b110cddf7e41dad7a649227ce5
Rahul Parmar <rahulparmar.n@gmail.com>
2020-08-16 16:37:13 +0530

    added login component

---
+++ react-external-login-spa/src/components/login/Login.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { UserManager } from 'oidc-client';
+import axios from 'axios';
+
+export function getClientSettings() {
+    return {
+        authority: 'http://localhost:5000',
+        client_id: 'angular_spa',
+        redirect_uri: 'http://localhost:4200/auth-callback',
+        post_logout_redirect_uri: 'http://localhost:4200/',
+        response_type: "id_token token",
+        scope: "openid profile email api.read",
+        filterProtocolClaims: true,
+        loadUserInfo: true,
+        automaticSilentRenew: true,
+        silent_redirect_uri: 'http://localhost:4200/silent-refresh.html'
+    };
+}
+
+var config = {
+    authority: "https://localhost:5001",
+    client_id: "spa_example",
+    redirect_uri: "https://localhost:5003/callback.html",
+    response_type: "code",
+    scope: "openid profile api1",
+    post_logout_redirect_uri: "https://localhost:5003/index.html",
+};
+
+var mgr = new UserManager(config);
+
+function Login() {
+
+    const handleGoogleLogin = () => {
+
+        axios.get('https://localhost:5001/api/externalauth2?provider=google&returnUrl=https://localhost:3000')
+            .then((response) => {
+                debugger
+            })
+            .catch((res) => {
+                debugger
+            })
+        /*
+        As we stated earlier, this end point will accept the GET 
+        requests originated from our AngularJS app, 
+        so it will accept GET request on the form:
+         http://ngauthenticationapi.azurewebsites.net/api/Account/ExternalLogin?
+         provider=Google&response_type=token&client_id=ngAuthApp&
+         redirect_uri=http://ngauthenticationweb.azurewebsites.net/authcomplete.html
+
+         /api/Account/ExternalLogin?
+         provider=Google&
+         response_type=token&
+         client_id=self&
+         redirect_uri=https://localhost/&
+         state=as..aaa
+         */
+    }
+
+    return (
+        <div>
+            <button onClick={handleGoogleLogin}> Sign in with Google </button>
+        </div>
+    );
+}
+
+export default Login;
