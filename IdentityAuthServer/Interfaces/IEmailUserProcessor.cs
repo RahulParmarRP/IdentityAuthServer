2bc5144008cbdb592619fc0ac2818baed358c187
Rahul Parmar <rahulparmar.n@gmail.com>
2020-08-31 15:26:55 +0530

    added interfaces

---
+++ IdentityAuthServer/Interfaces/IEmailUserProcessor.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
+
+namespace IdentityAuthServer.Interfaces
+{
+    public interface IEmailUserProcessor
+    {
+        Task<GrantValidationResult> ProcessAsync(Payload userInfoPayload, string email, string provider);
+    }
+}
---
+++ IdentityAuthServer/Interfaces/INonEmailUserProcessor.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
+
+namespace IdentityAuthServer.Interfaces
+{
+    public interface INonEmailUserProcessor
+    {
+        Task<GrantValidationResult> ProcessAsync(Payload userInfoPayload, string provider);
+    }
+}
